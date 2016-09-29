64310abd3d0343411dabb907a4a0e13a467c9a81
terminatorbot <terminator.robot.2016@gmail.com>
2016-09-29 04:38:35 -0700

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,6 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '211013807:AAGjwZF8IKKWCJAvlKnRfyTgRJ6e9Npxu5Y
+');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +116,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = 94413864;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
